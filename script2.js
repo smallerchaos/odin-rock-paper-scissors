025cefdc887445f133efd045a2c4d278efc6f1df
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-02-01 20:56:01 +0100

    Add ability to obtain and validate player choice

--- script2.js
+++ script2.js
@@ -2,6 +2,7 @@
 
 let computerChoice;
 let playerChoice;
+let isPlayerChoiceValid = false;
 
 // SET computer choice of rps
 
@@ -15,3 +16,27 @@ if (computerChoice == 1) {
     computerChoice = "scissors";
 }
 console.log(computerChoice);
+
+// OBTAIN player choice for rps
+
+while (isPlayerChoiceValid == false) {
+    playerChoice = prompt('Please choose "rock", "paper", or "scissors".');
+    console.log("playerChoice = " + playerChoice + ' which is a type of ' + typeof(playerChoice));
+
+    if (playerChoice == null ||
+    playerChoice == undefined ||
+    playerChoice == "" || isNaN(playerChoice) == false) {
+        console.log("wut");
+        alert(`Hmm... try again...`);
+        isPlayerChoiceValid = false;
+    } else if (String(playerChoice.toLowerCase()) == "rock" ||
+        String(playerChoice.toLowerCase()) == "paper" ||
+        String(playerChoice.toLowerCase()) == "scissors") {
+            console.log("Yay! Thanks for picking something!");
+            playerChoice = playerChoice.toLowerCase();
+            isPlayerChoiceValid = true;
+    } else {
+        alert(`That's not one of the choices...`);
+        isPlayerChoiceValid = false;
+    }
+}
